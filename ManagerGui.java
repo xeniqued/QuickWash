938eaa913128218dca675b6e97b951089960c33d
Aja Jazene <113658517+AjaJazene@users.noreply.github.com>
2023-12-01 10:26:19 -0500

    Update ManagerGui.java

--- ManagerGui.java
+++ ManagerGui.java
@@ -54,7 +54,7 @@ public class ManagerGui extends JFrame {
         logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                logOut();
+               new NotificationGUI().setVisible(true);
             }
         });
 
@@ -202,11 +202,7 @@ public class ManagerGui extends JFrame {
         JOptionPane.showMessageDialog(this, "Income Report Generated");
     }
 
-    private void logOut() {
-        new WelcomeScreen().setVisible(true);
-        dispose();
-    }
-
+ 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
